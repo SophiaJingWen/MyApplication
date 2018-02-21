955b493e9b9f0cb50192a99c4a780d7ee618ecdf
DaiSmallEast <970439131@qq.com>
2018-02-21 13:37:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dell/myapplication/ExploreActivity.java
@@ -0,0 +1,13 @@
+package com.example.dell.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExploreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore);
+    }
+}
---
+++ app/src/main/java/com/example/dell/myapplication/Task_Home.java
@@ -0,0 +1,13 @@
+package com.example.dell.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Task_Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_home);
+    }
+}
